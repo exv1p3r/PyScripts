5ce6d4eadde3fef82010a3694c4865b2d9c21c66
exv1p3r <edsoncervantes@protonmail.com>
2016-12-20 18:31:53 -0600

    Quit the banner from the script

--- ProFTPd_cmdExe.py
+++ ProFTPd_cmdExe.py
@@ -8,14 +8,6 @@
 import socket
 import sys
 import requests
-#Banner
-banner = ""
-banner += "  ___        __        ____                 _    _  \n" 
-banner +=" |_ _|_ __  / _| ___  / ___| ___ _ __      / \  | |    \n"
-banner +="  | || '_ \| |_ / _ \| |  _ / _ \ '_ \    / _ \ | |    \n"
-banner +="  | || | | |  _| (_) | |_| |  __/ | | |  / ___ \| |___ \n"
-banner +=" |___|_| |_|_|  \___/ \____|\___|_| |_| /_/   \_\_____|\n\n"
-print banner
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if(len(sys.argv) < 4):
     print '\n Usage : exploit.py server directory cmd'
