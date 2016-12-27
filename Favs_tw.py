2f92af52a45a4aacbd043a19ca028886ab03fd91
exv1p3r <edsoncervantes@protonmail.com>
2016-12-27 12:38:57 -0600

    Changed the query

--- Favs_tw.py
+++ Favs_tw.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python2
 import twitter
 apiTwitter = twitter.Api(consumer_key="sNbr6JF5MKBGCyYFv80NoW8lE", consumer_secret="pTwfMrlGHOZCbTc8LF1sFTDA9XwJnrQha6Avol7i17feDzo2ie", access_token_key="94489055-KDgQiLZYkPwmRvWuIUFYX7Y594sq35Bl6m20bdf4n", access_token_secret="G7JnBSfeUkevqsJqSYgEtsMuJN52FUO6OQ9HMTtQgZAMV")
-query = apiTwitter.GetSearch("Bruce Wayne", count=20)
+tw_query = raw_input("What's the Trending Topic you want to search?")
+query = apiTwitter.GetSearch(tw_query, count=20)
 for result in query:
     print "Tweet: %s " %(result.text)
     print "Creation date: %s " %(result.created_at)
@@ -9,4 +10,4 @@ for result in query:
     print "Language: %s " %(result.lang)
     print "Retweets count: %d " %(result.retweet_count)
     print "Account: %s " %(result.user.screen_name)
-    print "\n" 
+    print "\n"
