5a99b5e4ba47e91654aff807e7570079fcbb1119
exv1p3r <edsoncervantes@protonmail.com>
2017-07-25 04:46:20 +0000

    File to configure VLANS in a SW

---
+++ pyNet/pyTelnetS1_Scr1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+import getpass
+import sys
+import telnetlib
+
+HOST = "192.168.122.10"
+user = raw_input("Enter your telnet account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("Username: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+tn.write("enable\n")
+tn.write("cisco\n")
+tn.write("conf t\n")
+tn.write("vlan 10\n")
+tn.write("name Administracion\n")
+tn.write("vlan 20\n")
+tn.write("name Ingenieria\n")
+tn.write("vlan 30\n")
+tn.write("name Marketing\n")
+tn.write("vlan 40\n")
+tn.write("name Recursos_Humanos\n")
+tn.write("int loopback 0\n")
+tn.write("ip addr 1.1.1.1 255.255.255.0\n")
+tn.write("end\n")
+tn.write("exit\n")
+
+print tn.read_all()
