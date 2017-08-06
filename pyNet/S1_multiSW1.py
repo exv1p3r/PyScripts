a286dbefe635a0bbe69aa1511b361ccf6e6152df
exv1p3r <edsoncervantes@protonmail.com>
2017-08-05 23:12:13 -0500

    Configure multiple switches

---
+++ pyNet/S1_multiSW1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+import getpass
+import sys
+import telnetlib
+
+# Credentials needed to the telnet connection
+user = raw_input("Enter your telnet account: ")
+password = getpass.getpass()
+# Put the IP addrs to loop the conn
+for i in range(30, 36):
+    HOST = "192.168.122." + str(i)
+    # Connection using the telnetlib
+    tn = telnetlib.Telnet(HOST)
+    tn.read_until("Username: ")
+    tn.write(user + "\n")
+    if password:
+        tn.read_until("Password: ")
+        tn.write(password + "\n")
+    # Cisco configuration
+    tn.write("conf t\n")
+    for n in range(2, 10):
+        tn.write("vlan " + str(n) + "\n")
+        tn.write("name vlan_" + str(n) + "\n")
+    tn.write("end\n")
+    tn.write("exit\n")
+
+    print tn.read_all()
